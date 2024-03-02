12c0c5f7c1603bb4f6c77cbd0f3b7b5ebedfeb2a
Thiago Pires <thiagopac@gmail.com>
2024-03-02 15:03:45 -0300

    Solving injection issues

---
+++ press-hold-button-example/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'press-hold-button-example',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
